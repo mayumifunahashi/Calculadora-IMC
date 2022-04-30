801f7cbfb9d633f58ca5a860b6eade5b62fbf61a
Mayumi Funahashi <100293266+mayumifunahashi@users.noreply.github.com>
2022-04-30 15:00:40 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,7 @@
+const nome = document.getElementById('nome');
+const peso = document.getElementById('peso');
+const altura = document.getElementById('altura');
+
+const imc = peso /altura**
+
+function r
